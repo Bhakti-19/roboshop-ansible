a55f00875eda599b1f9ccb386bc533584b5cfc10
Bhakti-19 <saraf.bhakti@gmail.com>
2023-01-05 23:29:05 -0600

    create-roboshop-server-single-server.sh

--- roles/mysql/templates/root-pass.sql
+++ roles/mysql/templates/root-pass.sql
@@ -1,2 +1,8 @@
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${{ROOT_PASSWORD}}';
+--ALTER USER GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '{{ROOT_PASSWORD}}';
+--
+--create database if not exists cities;
+--GRANT ALL ON cities.* TO 'shipping'@'%' IDENTIFIED BY 'secret';
+--GRANT ALL ON cities.* TO 'shipping'@'localhost' IDENTIFIED BY 'secret';
+
 
